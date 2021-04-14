78b802f9ec5ed4facba207a3f3469cd6c4e5da86
5trange <adithmurali@protonmail.com>
2021-04-14 23:43:45 +0530

    Update ui_start.py
    Code cleanup.

--- ui_start.py
+++ ui_start.py
@@ -42,7 +42,6 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
-from PyQt5.QtCore import *
 from process import *
 from mainWindow import Ui_mainWindow #IMPORTING MAINWINDOW.PY
 
