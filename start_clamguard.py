fae930c5d6c9ad9a9ff8c7fe6b515730df8b6777
Strange <adithmurali@protonmail.com>
2021-05-04 17:05:57 +0530

    Code cleanup

--- start_clamguard.py
+++
@@ -1,7 +0,0 @@
-from subprocess import *
-print("Starting ClamAV Daemon.. Please wait..")
-clamav_daemon= Popen(['clamd.exe'], stdout = PIPE, encoding = 'utf-8')
-print("Starting ClamGuard WatchDog")
-from filesystemMonitor import *
-print("Starting ClamGuard")
-from ui_start import *
