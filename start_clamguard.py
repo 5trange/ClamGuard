e8bff03b46adb579b04afbea5108ae29e359e459
5trange <adithmurali@protonmail.com>
2021-05-08 20:23:28 +0530

    Added headers
    Added headers, marked ui_start.py as deprecated.

--- start_clamguard.py
+++ start_clamguard.py
@@ -1,3 +1,27 @@
+# -----------------------------------------------------------------------------
+# Name          :   start_clamguard.py
+# Product       :   ClamGuard
+# Authors       :   Adith, Bilal, Vinayak
+# Created       :   May-08-2021
+# Last updated  :   May-08-2021
+# -----------------------------------------------------------------------------
+# Licence:
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# -----------------------------------------------------------------------------
+
+# Imports
 import sys
 import platform
 import time
