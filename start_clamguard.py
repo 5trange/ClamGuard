4de9c6fe08c9dd35cf428fa567e28da3119b3976
5trange <adithmurali@protonmail.com>
2021-05-08 20:42:29 +0530

    Update start_clamguard.py

--- start_clamguard.py
+++ start_clamguard.py
@@ -34,16 +34,15 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from subprocess import *
-from mainWindow import Ui_mainWindow # Importing mainWindow.py
-from SplashScreen import Ui_SplashScreen # Importing SplashScreen.py
-
+from mainWindow import Ui_mainWindow  # Importing mainWindow.py
+from SplashScreen import Ui_SplashScreen  # Importing SplashScreen.py
 
 # Globals vars + env path
 appdata_dir = os.environ['APPDATA']
 win_dir = os.environ['SYSTEMROOT']
 root_drive = os.environ['SYSTEMDRIVE']
-drivers_dir = win_dir+'\\System32\\Drivers\\'
-system32_dir = win_dir+'\\System32\\'
+drivers_dir = win_dir + '\\System32\\Drivers\\'
+system32_dir = win_dir + '\\System32\\'
 
 
 # Splashscreen class
@@ -71,7 +70,7 @@ class SplashScreen(QMainWindow):
         self.timer.timeout.connect(self.progress)
         # Timer in milliseconds
         self.timer.start(35)
-        self.center() #CENTERING
+        self.center()  # CENTERING
         self.show()
 
     def progress(self):
@@ -86,7 +85,7 @@ class SplashScreen(QMainWindow):
             # Stop timer
             self.timer.stop()
             self.close()
-            self.main_window=MainWindow()
+            self.main_window = MainWindow()
             self.main_window.show()
 
         # Increase counter
@@ -98,12 +97,13 @@ class SplashScreen(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+
 # MainWindow class
 class MainWindow(QMainWindow):
     def __init__(self):
         # Shadow+Other functions
         QMainWindow.__init__(self)
-        self.ui=Ui_mainWindow()
+        self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
         self.shadow = QGraphicsDropShadowEffect(self)
         self.shadow.setBlurRadius(20)
@@ -111,12 +111,12 @@ class MainWindow(QMainWindow):
         self.shadow.setYOffset(0)
         self.shadow.setColor(QColor(0, 0, 0, 100))
         self.ui.dropshadowFrame.setGraphicsEffect(self.shadow)
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint) # Setting frameless window
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground) # Setting frame border
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # Setting frameless window
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # Setting frame border
 
         # Window control buttons
-        self.ui.minButton.clicked.connect(lambda: self.showMinimized()) # Minimize on click
-        self.ui.closeButton.clicked.connect(lambda: self.close()) # Close on click
+        self.ui.minButton.clicked.connect(lambda: self.showMinimized())  # Minimize on click
+        self.ui.closeButton.clicked.connect(lambda: self.close())  # Close on click
 
         # Titlebar dragging
         self.ui.titleBar.mouseMoveEvent = self.moveWindow
@@ -148,11 +148,10 @@ class MainWindow(QMainWindow):
         # To homepage from quarantine
         self.ui.quarantineHomeButton.clicked.connect(self.switch_home)
 
-
         # Scan page functions
         self.ui.quickscanButton.clicked.connect(self.launch_quickscan)
-        #self.ui.fullscanButton.clicked.connect(self.start_fullscan)
-        #self.ui.customscanButton.clicked.connect(self.start_customscan)
+        # self.ui.fullscanButton.clicked.connect(self.start_fullscan)
+        # self.ui.customscanButton.clicked.connect(self.start_customscan)
         self.ui.cancelscanButton.setEnabled(False)
         self.ui.cancelscanButton.clicked.connect(self.stop_scan)
 
@@ -161,17 +160,15 @@ class MainWindow(QMainWindow):
         self.ui.cancelUpdate.setEnabled(False)
         self.ui.cancelUpdate.clicked.connect(self.stop_update)
 
-
         # Quarantine functions
         self.ui.quarantineRefresh.clicked.connect(self.start_quarantine)
-
 
     # Mouse drag event handler
     def moveWindow(self, event):
         if event.buttons() == Qt.LeftButton:
-                self.move(self.pos() + event.globalPos() - self.dragPos)
-                self.dragPos = event.globalPos()
-                event.accept()
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
 
     def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
@@ -200,8 +197,8 @@ class MainWindow(QMainWindow):
         self.ui.customscanButton.setEnabled(False)
         self.ui.homeButton.setEnabled(False)
         self.ui.scanStatus.clear()
-        self.ui.scanStatus.appendPlainText("Quick scan started. Please wait...\n\
-        NOTE: Quick scan is very CPU Intensive, It is recommended to close all programs before scanning.")
+        self.ui.scanStatus.appendPlainText(
+            "Quick scan started. Please wait...\n\nNOTE: Quick scan is very CPU Intensive, It is recommended to close all programs before scanning.")
         self.sthread = QuickScan()
         self.sthread.ret.connect(self.set_scan_value)
         self.sthread.start()
@@ -215,7 +212,7 @@ class MainWindow(QMainWindow):
         self.ui.scanStatus.appendPlainText(scanstring)
 
     def stop_scan(self):
-        self.SingleThread = threading.Thread(target = self.stop_scan_thread)
+        self.SingleThread = threading.Thread(target=self.stop_scan_thread)
         self.SingleThread.start()
 
     def stop_scan_thread(self):
@@ -242,7 +239,7 @@ class MainWindow(QMainWindow):
         self.ui.updateStatus.appendPlainText(updatestring)
 
     def stop_update(self):
-        self.SingleThread = threading.Thread(target = self.stop_update_thread)
+        self.SingleThread = threading.Thread(target=self.stop_update_thread)
         self.SingleThread.start()
 
     def stop_update_thread(self):
@@ -253,24 +250,25 @@ class MainWindow(QMainWindow):
 
     # Quarantine file population threads.
     def start_quarantine(self):
-        self.QuarantineThread = threading.Thread(target = self.populate_quarantine)
+        self.QuarantineThread = threading.Thread(target=self.populate_quarantine)
         self.QuarantineThread.start()
 
     def populate_quarantine(self):
         WorkingDirectory = os.getcwd()
-        QuarantineDirectory = WorkingDirectory+"\\quarantine\\"
+        QuarantineDirectory = WorkingDirectory + "\\quarantine\\"
         lines = os.listdir(QuarantineDirectory)
         entries = 0
         for line in lines:
             if entries == 0:
                 self.ui.quarantineView.setRowCount(0)
             self.ui.quarantineView.insertRow(entries)
-            self.ui.quarantineView.setItem(entries,0,QTableWidgetItem(line))
-            entries = entries+1
+            self.ui.quarantineView.setItem(entries, 0, QTableWidgetItem(line))
+            entries = entries + 1
 
-    #Opening github repo page.
+    # Opening github repo page.
     def openGithub(self, event):
         webbrowser.open("https://github.com/5trange/ClamGuard")
+
 
 class Updater(QThread):
     ret = Signal(str)
@@ -281,9 +279,9 @@ class Updater(QThread):
 
     def run(self):
         try:
-            self.process =  Popen(['freshclam.exe'], stdout = PIPE, encoding = 'utf-8')
+            self.process = Popen(['freshclam.exe'], stdout=PIPE, encoding='utf-8')
             while self.process.poll() is None:
-                if(self.abort == True):
+                if (self.abort == True):
                     try:
                         self.ret.emit("\n\nStopping update...")
                         break
@@ -295,12 +293,13 @@ class Updater(QThread):
                     if self.updatebuffer != '':
                         self.ret.emit(self.updatebuffer)
 
-            if(self.abort == False):
+            if (self.abort == False):
                 self.ret.emit("\n\nDatabase refreshed.")
-            elif(self.abort == True):
+            elif (self.abort == True):
                 self.ret.emit("\n\nDatabase update cancelled.")
         except Exception as f:
             print(f"Something happened. Error code: {e}")
+
 
 class QuickScan(QThread):
     ret = Signal(str)
@@ -308,9 +307,11 @@ class QuickScan(QThread):
 
     def run(self):
         try:
-            self.process = Popen(['clamdscan.exe',appdata_dir,drivers_dir,'--infected','--multiscan','--move=quarantine'], stdout = PIPE, encoding = 'utf-8')
+            self.process = Popen(
+                ['clamdscan.exe', appdata_dir, drivers_dir, '--infected', '--multiscan', '--move=quarantine'],
+                stdout=PIPE, encoding='utf-8')
             while self.process.poll() is None:
-                if(self.abort == True):
+                if (self.abort == True):
                     try:
                         self.ret.emit("\n\nStopping scan...")
                         break
@@ -322,15 +323,15 @@ class QuickScan(QThread):
                     if self.scanbuffer != '':
                         self.ret.emit(self.scanbuffer)
 
-            if(self.abort == False):
+            if (self.abort == False):
                 self.ret.emit("\n\nQuick scan complete.")
-            elif(self.abort == True):
+            elif (self.abort == True):
                 self.ret.emit("\n\nScan cancelled.")
-        except Exception as f:
+        except Exception as e:
             print(f"Something happened. Error code: {e}")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = SplashScreen()
     sys.exit(app.exec_())
