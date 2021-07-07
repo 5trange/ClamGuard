276e11545b69a22d0ef8afcea7c8fdca58769f38
5trange <adithmurali@protonmail.com>
2021-07-07 23:55:05 +0530

    Update usbmon.py

--- usbmon.py
+++ usbmon.py
@@ -4,12 +4,6 @@ def diff(list1, list2):
     list_difference = [item for item in list1 if item not in list2]
     return list_difference
 
-def foo():
-    print("New dive introduced")
-
-def ham():
-    print("Drive disconnected")
-
 dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
 print(drives)
@@ -20,11 +14,9 @@ while True:
         newdrive = str(x)[1:-1]
         newdrive = newdrive.replace("'",'')
         print("New drives:     " + newdrive)
-        foo()
     x = diff(drives, uncheckeddrives)
     if x:
         remdrive = str(x)[1:-1]
         remdrive = remdrive.replace("'",'')
         print("Removed drives: " + remdrive)
-        ham()
     drives = uncheckeddrives
