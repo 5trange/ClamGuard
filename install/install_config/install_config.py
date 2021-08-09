59ae436727624d60b105b0bbc00e3cf0901eec51
5trange <adithmurali@protonmail.com>
2021-08-09 08:42:38 +0530

    Updated install_config.py
    Added more system variables into the config file. These variables are replaced with the actual values using the install_config.py script.

--- install/install_config/install_config.py
+++ install/install_config/install_config.py
@@ -1,18 +1,23 @@
 # This gets executed during ClamGuard's install process to update the config file according to the system setup.
+# Replaces the variable %DBLOC% with actual database location.
+# Also can be used to set the location of log files.
 import sys
 import fileinput
 import os
 
 # System vars
 program_data = os.environ['PROGRAMDATA']
+program_data.replace("/","\\")
 dbloc = program_data + '\.clamguard\db'
-dbloc.replace("/","\\")
+clamdloc = program_data + '\.clamguard\log\clamguard_clamd.log'
+freshloc = program_data + '\.clamguard\log\clamguard_fresh.log'
 
 # clamd.conf
 with open('clamd.conf', 'r') as file:
   data = file.read()
 
 data = data.replace('%DBLOC%', dbloc)
+data = data.replace('%LOGLOC%', clamdloc)
 
 with open('clamd.conf', 'w') as file:
   file.write(data)
@@ -22,6 +27,7 @@ with open('freshclam.conf', 'r') as file:
   data = file.read()
 
 data = data.replace('%DBLOC%', dbloc)
+data = data.replace('%LOGLOC%', freshloc)
 
 with open('freshclam.conf', 'w') as file:
   file.write(data)
