8bf1da541d1571317fae3547909a0ceb8e3bbecc
5trange <64513428+5trange@users.noreply.github.com>
2023-09-11 16:29:44 +0530

    Create an clamav engine

--- engine/src/main.zig
+++ engine/src/main.zig
@@ -9,9 +9,25 @@ pub fn main() !void {
     // Init ClamAV. If this is successful, cl_init returns CL_SUCCESS
     var c_init_status: c_uint = clamav.cl_init(clamav.CL_INIT_DEFAULT);
 
-    if (c_init_status == clamav.CL_SUCCESS) {
-        try stdout.print("SUCCESSFULLY INITIALIZED LIBCLAMAV\n", .{});
-    } else {
+    if (c_init_status != clamav.CL_SUCCESS) {
+        // Failed to init ClamAV.
         try stdout.print("FAILED TO INITIALIZE LIBCLAMAV\n", .{});
+        return;
     }
+    try stdout.print("SUCCESSFULLY INITIALIZED LIBCLAMAV\n", .{});
+
+    // Create a new engine.
+    var engine: ?*clamav.cl_engine = null;
+    engine = clamav.cl_engine_new();
+
+    if (engine == null) {
+        try stdout.print("FAILED TO CREATE AN ENGINE\n", .{});
+        return;
+    }
+    try stdout.print("SUCCESSFULLY CREATED AN ENGINE\n", .{});
+
+    // Print the default database directory.
+    try stdout.print("DEFAULT DATABASE DIRECTORY {any}\n", .{clamav.cl_retdbdir()});
 }
+
+pub fn create_engine() !void {}
