9a05cf32b84eb778582e532a2ec1ab68f75eb1fb
Bilal <bjlegend007@gmail.com>
2023-09-16 21:23:09 +0530

    added openssl support for windows

--- engine/build.zig
+++ engine/build.zig
@@ -30,13 +30,27 @@ pub fn build(b: *std.Build) void {
     switch (tag) {
         .windows => {
             const clamav_path = b.env_map.get("CLAMAV_DIR") orelse "C:\\clamav";
-            std.debug.print("clamav path is {s}", .{clamav_path});
+            std.debug.print("clamav path is {s}\n", .{clamav_path});
             const clamav_include_raw = std.fmt.allocPrint(b.allocator, "{s}\\include", .{clamav_path});
             const clamav_include = clamav_include_raw catch "C:\\clamav\\include";
             defer b.allocator.free(clamav_include);
-            exe.linkSystemLibrary("openssl");
+
             exe.addIncludePath(clamav_include);
             exe.addLibraryPath(clamav_path);
+
+            const openssl_path = b.env_map.get("OPENSSLDIR") orelse "C:\\openssl";
+            std.debug.print("openssl path is {s}\n", .{openssl_path});
+            const openssl_lib_raw = std.fmt.allocPrint(b.allocator, "{s}\\lib", .{openssl_path});
+            const openssl_lib = openssl_lib_raw catch "C:\\openssl\\include";
+            defer b.allocator.free(openssl_lib);
+            const openssl_include_raw = std.fmt.allocPrint(b.allocator, "{s}\\include", .{openssl_path});
+            const openssl_include = openssl_include_raw catch "C:\\openssl\\include";
+            defer b.allocator.free(openssl_include);
+
+            exe.addLibraryPath(openssl_lib);
+            exe.addIncludePath(openssl_include);
+
+            exe.linkSystemLibrary("libssl");
         },
         else => {},
     }
