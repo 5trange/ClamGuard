16a707d6ed377dc504203808ad3c3f416c0198c5
Bilal <bjlegend007@gmail.com>
2026-07-21 22:06:45 +0530

    updated the build settings

--- build.py
+++ build.py
@@ -1,11 +1,24 @@
-# This File exist to build the project data use this file before running the project
+# This File exist to build the resource data use this file before running the program
 
 import subprocess
-from pathlib import Path
+import argparse
 
-if not Path("src/resources/resources_rc.py").is_file():
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Manage the ClamGuard project")
+
+    subparsers = parser.add_subparsers(dest="command", required=True, help="Available subcommands")
+    build_parser = subparsers.add_parser("build", help="Build the project using PyInstaller")
+    build_parser.add_argument("--no-resources", action="store_true", help="Skip resources building step")
+
+    return parser.parse_args()
+
+
+def build_resources():
     process = subprocess.Popen(
         [
+            "uv",
+            "run",
             "pyside6-rcc",
             "./resources/resources.qrc",
             "-o",
@@ -15,3 +28,33 @@ if not Path("src/resources/resources_rc.py").is_file():
     return_code = process.wait()
     if return_code != 0:
         print("Error : Running the pyside6-rcc command")
+
+def build_executable():
+    process = subprocess.Popen(
+        [
+            "uv",
+            "run",
+            "pyinstaller",
+            "src/main.py",
+            "--onefile",
+            "--name",
+            "ClamGuard",
+            "--icon",
+            "resources/icon.ico",
+            "--distpath",
+            "dist/"
+        ]
+    )
+    return_code = process.wait()
+    if return_code != 0:
+        print("Error : Running the pyinstaller command")
+
+def main():
+    args = parse_args()
+    if not args.no_resources:
+        build_resources()
+    if args.command == "build":
+        build_executable()
+
+if __name__ == "__main__":
+    main()
